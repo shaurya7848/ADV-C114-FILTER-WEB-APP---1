e4f706f6663c5bf7b0d3b043c0ce80c112bbc1e7
shaurya7848 <87648314+shaurya7848@users.noreply.github.com>
2022-06-19 16:39:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function reload(){
+
+}
+
+function draw(){
+canvas = createCanvas(300 , 300);
+canvas.center();
+}
+
+function setup(){
+
+}
+
+function take_snapshot(){
+    save("filteredimage.png");
+}
